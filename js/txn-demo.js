bc4ef2bc64d61f8ece18aa5bd311401795ec078d
Royal Fork <rf@royalforkblog.com>
2014-10-01 02:57:23 +0200

    Generate testnet address

--- js/txn-demo.js
+++ js/txn-demo.js
@@ -1,1 +1,23 @@
-console.log("this is the txn demo");
+function init() {
+  $("#txndemo-passphrase").on("input", passphraseChanged);
+}
+
+function passphraseChanged () {
+  var val = $("#txndemo-passphrase").val();
+
+  if (val === ""){
+    $("#txndemo-priv-key").text("N/A");
+    $("#txndemo-address").text("N/A");
+    return; 
+  }
+
+  // change phrase into private key
+  var priv = bitcoin.crypto.sha256(val);
+  $("#txndemo-priv-key").text(priv.toString("hex"));
+
+  // change address
+  key = bitcoin.ECKey.fromBuffer(priv);
+  $("#txndemo-address").text(key.pub.getAddress(bitcoin.networks.testnet).toString());
+}
+
+$(document).ready(init);
